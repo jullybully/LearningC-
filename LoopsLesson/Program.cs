8c1f68599e33d02083a2d9d7b52b37b5973c3227
Gvidas Pocius <gvpocius97@gmail.com>
2023-04-05 09:11:45 +0300

    Loops
    for (a; b)
    {
    code
    }
    
    foreach (a)
    {
    code
    }
    
    do {
    code
    } while (a)

---
+++ LoopsLesson/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 10 IMDb movies array
+            string[] topMovies = { "The Shawshank Redemption", "The Godfather", "The Godfather: Part II", "The Dark Knight", "12 Angry Men", "Schindler's List", "The Lord of the Rings: The Return of the King", "Pulp Fiction", "The Lord of the Rings: The Fellowship of the Ring", "Forrest Gump" };
+
+            // Loop until the user enters "exit"
+            while (true)
+            {
+                // Enter a movie
+                Console.Write("Enter a movie to watch (or type 'exit' to quit): ");
+                string movie = Console.ReadLine();
+
+                if (movie.ToLower() == "exit")
+                {
+                    // Exit the program if the user enters "exit"
+                    break;
+                }
+                else if (Array.IndexOf(topMovies, movie) == -1)
+
+
+                            // For Loop
+                            // Also should be else not else if
+                            //bool foundMovie = false;
+                            //for (int i = 0; i < topMovies.Length; i++)
+                            //{
+                               // if (topMovies[i].Equals(movie, StringComparison.OrdinalIgnoreCase))
+                               // {
+                                   // foundMovie = true;
+                                   // break;
+                              //  }
+                           // }
+                           // Then i will check with if statement if movie founded and else if it didn't founded will give results.
+
+                            {
+                    // Display a message if the movie is not in the list
+                    Console.WriteLine("Sorry, we don't have that movie.");
+                }
+                else
+                {
+                    // Display a message if the movie is in the list
+                    Console.WriteLine("Great choice! Enjoy watching " + movie + ".");
+                }
+            }
+        }
+    }
+}
---
+++ SecondLesson/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random Number Generator");
+
+            // Prompt the user for the minimum and maximum values
+            Console.Write("Enter the minimum value: ");
+            int min = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the maximum value: ");
+            int max = int.Parse(Console.ReadLine());
+
+            // Prompt the user for additional options
+            Console.Write("Allow negative values? (y/n): ");
+            bool allowNegative = Console.ReadLine().ToLower() == "y";
+
+            Console.Write("Allow decimal values? (y/n): ");
+            bool allowDecimal = Console.ReadLine().ToLower() == "y";
+
+            // Create a random number generator
+            Random rand = new Random();
+
+            // Generate a random number within the specified range
+            double randomNum = 0;
+
+            if (allowDecimal)
+            {
+                randomNum = (rand.NextDouble() * (max - min)) + min;
+            }
+            else
+            {
+                randomNum = rand.Next(min, max + 1);
+            }
+
+            if (!allowNegative && randomNum < 0)
+            {
+                randomNum = 0;
+            }
+
+            // Display the random number
+            Console.WriteLine($"Your random number is: {randomNum}");
+            Console.ReadLine();
+        }
+    }
+}
